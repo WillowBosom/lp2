4eb2a9ac4a530722627bca6c3274fc16fb9e2a78
Caio Lopes <bigrubano@yahoo.com.br>
2016-03-04 11:00:19 -0300

    Exercicio 2, topico 3
    Ainda não está pronto.

---
+++ exercicio2/Exercicio1_3.java
@@ -0,0 +1,28 @@
+public class ContaBancaria2{
+	int agencia, contaCorrente, saldo, limiteExtra;
+	String titularConta;
+	public void ImprimirSaldo(){
+		
+	}
+	public void ImprimirSaldoTotal(){
+		
+	}
+	public void imprimirAgencia{
+		System.out.print("A agencia é: " + agencia);
+	}
+	public void imprimirContaCorrente{
+		System.out.print("A conta corrente é: " + contaCorrente);
+	}
+	public void imprimirContaCorrente{
+		System.out.print("A conta corrente é: " + contaCorrente);
+	}
+	public static void main(String[] args){
+		ContaBancaria2 conta1 = new ContaBancaria2();
+		conta1.agencia = 3610;
+		conta1.contaCorrente = 15766;
+		ContaBancaria2 conta2 = new ContaBancaria2();
+		conta2.agencia = 810;
+		conta1.contaCorrente = 6132;
+		
+	}
+}
