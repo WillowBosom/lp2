48c5088214e9dfc58e6a8a170167448d41ae2492
Caio Lopes <bigrubano@yahoo.com.br>
2016-03-04 10:35:04 -0300

    Exercicio 1, topico 4

---
+++ exercicio1/Exercicio1_4.java
@@ -0,0 +1,15 @@
+public class Exercicio1_4{
+	public static void main(String[] args){
+				float a = 45, b = 20, c = 3;
+				float M, D, Su, So;
+				M = a * b * c;
+				D = a / b / c;
+				Su = a - b - c;
+				So = a + b + c;
+				System.out.println("O valor da Multiplicação foi: " + M);
+				System.out.println("O valor da Divisão foi: " + D);
+				System.out.println("O valor da Subtração foi: " + Su);
+				System.out.println("O valor da Soma foi: " + So);
+	}
+	
+}
